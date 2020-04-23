eba49d4831ffb34af08a2ff17616df264120ea06
Eli Thompson <eli@eliandlyndi.com>
2020-04-23 13:32:31 -0700

    adding a working project

---
+++ Sdk/SdkTests/SdkTests.swift
@@ -0,0 +1,35 @@
+//
+//  SdkTests.swift
+//  SdkTests
+//
+//  Created by Eli Thompson on 4/23/20.
+//  Copyright © 2020 Spreedly. All rights reserved.
+//
+
+import XCTest
+@testable import Sdk
+
+class SdkTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(false)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
