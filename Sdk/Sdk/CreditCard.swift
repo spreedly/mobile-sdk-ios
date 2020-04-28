ab61fd64220cc89d4362ab09d156b317182b1f8f
Eli Thompson <eli@eliandlyndi.com>
2020-04-28 12:16:20 -0700

    add CreditCardPaymentMethodResponse and friends

--- Sdk/Sdk/CreditCard.swift
+++ Sdk/Sdk/CreditCard.swift
@@ -180,3 +180,143 @@ public struct CreateCreditCardRequest: Encodable {
         self.paymentMethod = paymentMethod
     }
 }
+
+enum PaymentMethodResponse {
+    case creditCard(CreditCardPaymentMethodResponse)
+    case thirdPartyToken(Int)
+}
+
+public class PaymentMethodResponseBase: Decodable {
+    public var token: String
+    public var createdAt: Date
+    public var updatedAt: Date
+    public var email: String?
+    public var data: [String:Decodable]?
+    public var storageState: String
+    public var test: Bool
+    public var metadata: [String:Decodable]?
+    public var callbackUrl: String?
+    public var paymentMethodType: String
+    public var errors: [String]?
+
+    enum CodingKeys: String, CodingKey {
+        case token
+        case createdAt
+        case updatedAt
+        case email
+        case data
+        case storageState
+        case test
+        case metadata
+        case callbackUrl
+        case paymentMethodType
+        case errors
+    }
+
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        token = try container.decode(String.self, forKey: CodingKeys.token)
+        createdAt = try container.decode(Date.self, forKey: CodingKeys.createdAt)
+        updatedAt = try container.decode(Date.self, forKey: CodingKeys.updatedAt)
+        email = try container.decode(String.self, forKey: CodingKeys.email)
+//        self.data = try container.decode([String:Decodable]?.self, forKey: CodingKeys.data)
+        data = Dictionary<String, Decodable>()
+        storageState = try container.decode(String.self, forKey: CodingKeys.storageState)
+        test = try container.decode(Bool.self, forKey: CodingKeys.test)
+//        self.metadata = try container.decode([String:Decodable]?.self, forKey: CodingKeys.metadata)
+        metadata = Dictionary<String, Decodable>()
+        callbackUrl = try container.decodeIfPresent(String.self, forKey: CodingKeys.callbackUrl)
+        paymentMethodType = try container.decode(String.self, forKey: CodingKeys.paymentMethodType)
+        errors = try container.decode([String]?.self, forKey: CodingKeys.errors)
+    }
+}
+
+public class CreditCardPaymentMethodResponse: PaymentMethodResponseBase {
+    public var lastFourDigits: String
+    public var firstSixDigits: String
+    public var cardType: String
+    public var firstName: String
+    public var lastName: String
+    public var address1: String?
+    public var address2: String?
+    public var city: String?
+    public var state: String?
+    public var zip: String?
+    public var country: String?
+    public var phoneNumber: String?
+    public var company: String?
+    public var fullName: String?
+    public var eligibleForCardUpdater: Bool
+    public var shippingAddress1: String?
+    public var shippingAddress2: String?
+    public var shippingCity: String?
+    public var shippingState: String?
+    public var shippingZip: String?
+    public var shippingCountry: String?
+    public var shippingPhoneNumber: String?
+    public var fingerprint: String
+    public var number: String
+    public var verificationValue: String
+
+    enum CodingKeys: String, CodingKey {
+        case lastFourDigits
+        case firstSixDigits
+        case cardType
+        case firstName
+        case lastName
+        case address1
+        case address2
+        case city
+        case state
+        case zip
+        case country
+        case phoneNumber
+        case company
+        case fullName
+        case eligibleForCardUpdater
+        case shippingAddress1
+        case shippingAddress2
+        case shippingCity
+        case shippingState
+        case shippingZip
+        case shippingCountry
+        case shippingPhoneNumber
+        case fingerprint
+        case number
+        case verificationValue
+    }
+
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        lastFourDigits = try container.decode(String.self, forKey: CodingKeys.lastFourDigits)
+        lastFourDigits = "9999"
+        firstSixDigits = try container.decode(String.self, forKey: CodingKeys.firstSixDigits)
+        cardType = try container.decode(String.self, forKey: CodingKeys.cardType)
+        firstName = try container.decode(String.self, forKey: CodingKeys.firstName)
+        lastName = try container.decode(String.self, forKey: CodingKeys.lastName)
+        address1 = try container.decode(String?.self, forKey: CodingKeys.address1)
+        address2 = try container.decode(String?.self, forKey: CodingKeys.address2)
+        city = try container.decode(String?.self, forKey: CodingKeys.city)
+        state = try container.decode(String?.self, forKey: CodingKeys.state)
+        zip = try container.decode(String?.self, forKey: CodingKeys.zip)
+        country = try container.decode(String?.self, forKey: CodingKeys.country)
+        phoneNumber = try container.decode(String?.self, forKey: CodingKeys.phoneNumber)
+        fullName = try container.decode(String?.self, forKey: CodingKeys.fullName)
+        eligibleForCardUpdater = try container.decode(Bool.self, forKey: CodingKeys.eligibleForCardUpdater)
+        shippingAddress1 = try container.decode(String?.self, forKey: CodingKeys.shippingAddress1)
+        shippingAddress2 = try container.decode(String?.self, forKey: CodingKeys.shippingAddress2)
+        shippingCity = try container.decode(String?.self, forKey: CodingKeys.shippingCity)
+        shippingState = try container.decode(String?.self, forKey: CodingKeys.shippingState)
+        shippingZip = try container.decode(String?.self, forKey: CodingKeys.shippingZip)
+        shippingCountry = try container.decode(String?.self, forKey: CodingKeys.shippingCountry)
+        shippingPhoneNumber = try container.decode(String?.self, forKey: CodingKeys.shippingPhoneNumber)
+        fingerprint = try container.decode(String.self, forKey: CodingKeys.fingerprint)
+        number = try container.decode(String.self, forKey: CodingKeys.number)
+        verificationValue = try container.decode(String.self, forKey: CodingKeys.verificationValue)
+        try super.init(from: decoder)
+    }
+}
+
+public struct ShowCreditCardResponse: Decodable {
+    public var paymentMethod: CreditCardPaymentMethodResponse
+}
