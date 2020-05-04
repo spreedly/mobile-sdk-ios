b176d97c0341f56f1d7c3b7c05fc6bcb6118a06a
Eli Thompson <eli@eliandlyndi.com>
2020-05-04 16:19:53 -0700

    move SpreedlyError to Errors

--- Sdk/Main.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public struct SpreedlyError: Error, CustomStringConvertible {
-    public var description: String {
-        "SpreedlyError: \(message)"
-    }
-
-     public let message: String
-}
