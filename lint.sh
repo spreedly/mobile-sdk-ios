e951549d6d387890538357cb84bb33431cef0a2e
Stefan Rusek <stefan@rusek.org>
2020-05-06 16:22:11 -0400

    linting

--- lint.sh
+++ lint.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
 pushd Sdk
-
-../Pods/SwiftLint/swiftlint autocorrect
-
+../Pods/SwiftLint/swiftlint
 popd
+pushd SdkTests
+../Pods/SwiftLint/swiftlint
+popd
+pushd SdkIntegraionTests
+../Pods/SwiftLint/swiftlint
+popd
+pushd Sample
+../Pods/SwiftLint/swiftlint
+popd
+
