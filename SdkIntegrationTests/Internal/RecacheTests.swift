7c6d72965023c6d79f7a9e4fb628ed952fe548f8
Eli Thompson <eli@eliandlyndi.com>
2020-05-13 12:25:33 -0700

    RecacheTests use assertResult

--- SdkIntegrationTests/Internal/RecacheTests.swift
+++ SdkIntegrationTests/Internal/RecacheTests.swift
@@ -10,9 +10,8 @@ import RxSwift
 class RecacheTests: XCTestCase {
     func testCanRecache() throws {
         let creditCardPromise = try Helpers.createCreditCard(retained: true)
-        let expectation = self.expectation(description: "can recache verification value")
 
-        _ = creditCardPromise.flatMap { transaction -> Single<Transaction<CreditCardResult>> in
+        let transaction = try creditCardPromise.flatMap { transaction -> Single<Transaction<CreditCardResult>> in
             let creditCard = transaction.paymentMethod
             guard let token = creditCard?.token else {
                 return Single.error(TestError.invalidResponse(
@@ -22,14 +21,9 @@ class RecacheTests: XCTestCase {
 
             let verify = Helpers.createClient().createSecureString(from: Helpers.verificationValue)
             return Helpers.createClient().recache(token: token, verificationValue: verify)
-        }.subscribe(onSuccess: { (transaction: Transaction<CreditCardResult>) in
-            expectation.fulfill()
-            XCTAssertEqual("RecacheSensitiveData", transaction.transactionType)
-            XCTAssert(transaction.succeeded)
-        }, onError: { error in
-            expectation.fulfill()
-            XCTFail("\(error)")
-        })
-        self.wait(for: [expectation], timeout: 10.0)
+        }.assertResult(self)
+
+        XCTAssertEqual("RecacheSensitiveData", transaction.transactionType)
+        XCTAssert(transaction.succeeded)
     }
 }
