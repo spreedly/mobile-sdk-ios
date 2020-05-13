e5285903f796ecad66f76ef92280f9fb31a61cf1
Eli Thompson <eli@eliandlyndi.com>
2020-05-13 10:59:13 -0700

    split BankAccountTests to own file

--- SdkIntegrationTests/Internal/BankAccountTests.swift
+++ SdkIntegrationTests/Internal/BankAccountTests.swift
@@ -2,7 +2,41 @@
 // Created by Stefan Rusek on 5/6/20.
 //
 
-import Foundation
 import XCTest
 import RxSwift
 @testable import Sdk
+
+class BankAccountTests: XCTestCase {
+    func testCanCreateBankAccount() throws {
+        let client = Helpers.createClient()
+        let bankAccount = BankAccountInfo(
+                firstName: "Asha",
+                lastName: "Dog",
+                bankRoutingNumber: "021000021",
+                bankAccountNumber: client.createSecureString(from: "9876543210"),
+                bankAccountType: .checking,
+                bankAccountHolderType: .personal
+        )
+
+        let expectation = self.expectation(description: "can create bank account")
+        let promise = client.createBankAccountPaymentMethod(bankAccount: bankAccount, email: "asha@dog.com")
+
+        _ = promise.subscribe(onSuccess: { (transaction: Transaction<BankAccountResult>) in
+            XCTAssertNotNil(transaction)
+            let bankAccount = transaction.paymentMethod!
+            XCTAssertNotNil(bankAccount.token)
+            XCTAssertNil(bankAccount.bankName)
+            XCTAssertEqual(bankAccount.accountType, BankAccountType.checking)
+            XCTAssertEqual(bankAccount.accountHolderType, BankAccountHolderType.personal)
+            XCTAssertEqual(bankAccount.routingNumberDisplayDigits, "021")
+            XCTAssertEqual(bankAccount.accountNumberDisplayDigits, "3210")
+            XCTAssertEqual(bankAccount.routingNumber, "021*")
+            XCTAssertEqual(bankAccount.accountNumber, "*3210")
+            expectation.fulfill()
+        }, onError: { error in
+            XCTFail("\(error)")
+            expectation.fulfill()
+        })
+        self.wait(for: [expectation], timeout: 10.0)
+    }
+}
