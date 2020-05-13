9222c98b4a628b97a233cebd194d25a2df36db94
Eli Thompson <eli@eliandlyndi.com>
2020-05-13 10:54:39 -0700

    add Helpers for common integration test methods

---
+++ SdkIntegrationTests/Internal/Helpers.swift
@@ -0,0 +1,34 @@
+//
+// Created by Eli Thompson on 5/12/20.
+//
+
+import RxSwift
+@testable import Sdk
+
+class Helpers {
+    static let verificationValue = "919"
+
+    static func createClient() -> SpreedlyClientImpl {
+        let key = ProcessInfo.processInfo.environment["ENV_KEY"] ?? secretEnvKey
+        let secret = ProcessInfo.processInfo.environment["ENV_SECRET"] ?? secretEnvSecret
+        return SpreedlyClientImpl(envKey: key, envSecret: secret, test: true)
+    }
+
+    static func createCreditCard(retained: Bool? = nil) throws -> Single<Transaction<CreditCardResult>> {
+        let client = createClient()
+        let creditCard = CreditCardInfo(
+                firstName: "Dolly",
+                lastName: "Dog",
+                number: client.createSecureString(from: "4111111111111111"),
+                verificationValue: client.createSecureString(from: verificationValue),
+                year: 2029,
+                month: 1
+        )
+        creditCard.retained = retained
+
+        return client.createCreditCardPaymentMethod(
+                creditCard: creditCard,
+                email: "dolly@dog.com"
+        )
+    }
+}
