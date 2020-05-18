4219ef9afe4d15123d50b7bca1ead6fc4b6cb996
Stefan Rusek <stefan@rusek.org>
2020-05-18 11:32:45 -0400

    Sdk -> CoreSdk

--- CoreSdk/Sdk.h
+++ CoreSdk/Sdk.h
@@ -1,6 +1,6 @@
 //
-//  Sdk.h
-//  Sdk
+//  CoreSdk.h
+//  CoreSdk
 //
 //  Created by Eli Thompson on 4/23/20.
 //  Copyright © 2020 Spreedly. All rights reserved.
@@ -8,12 +8,12 @@
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for Sdk.
+//! Project version number for CoreSdk.
 FOUNDATION_EXPORT double SdkVersionNumber;
 
-//! Project version string for Sdk.
+//! Project version string for CoreSdk.
 FOUNDATION_EXPORT const unsigned char SdkVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Sdk/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <CoreSdk/PublicHeader.h>
 
 
