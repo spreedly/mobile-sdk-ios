abed3250c6faf5bee13f450e058dc5e008f1176b
Eli Thompson <eli@eliandlyndi.com>
2020-05-20 12:27:18 -0700

    wip

--- CocoaSdk/SPSecureForm.swift
+++ CocoaSdk/SPSecureForm.swift
@@ -15,20 +15,51 @@ public class SPSecureForm: UIView {
 
     public var delegate: SPSecureFormDelegate?
 
+    public var client: SpreedlyClient
+
     public var creditCardDefaults: CreditCardInfo?
     public var bankAccountDefaults: BankAccountInfo?
 
     @IBOutlet public weak var fullName: UITextField?
     @IBOutlet public weak var creditCardNumber: SPSecureTextField?
     @IBOutlet public weak var creditCardVerificationNumber: SPSecureTextField?
+    @IBOutlet public weak var expirationMonth: UITextField?
+    @IBOutlet public weak var expirationYear: UITextField?
 
-    @IBAction public func createCreditCardPaymentMethod(sender: UIView) -> Single<Transaction<CreditCardResult>> {
-        let alert = UIAlertController(title: "Submit", message: "\(creditCardNumber?.text() ?? "") - \(creditCardVerificationNumber?.text() ?? "")", preferredStyle: .alert)
+    public override init(frame: CGRect) {
+        client = createSpreedlyClient(envKey: "secretEnvKey", envSecret: "secretEnvSecret")
+        super.init(frame: frame)
+    }
+
+    public required init?(coder: NSCoder) {
+        client = createSpreedlyClient(envKey: "secretEnvKey", envSecret: "secretEnvSecret")
+        super.init(coder: coder)
+    }
+
+
+    @IBAction public func createCreditCardPaymentMethod(sender: UIView) {
+        let message = """
+                      \(fullName?.text() ?? "No name")
+                      \(creditCardNumber?.text() ?? "")
+                      \(creditCardVerificationNumber?.text() ?? "")
+                      \(expirationMonth?.text() ?? "mm")/\(expirationYear?.text() ?? "yy")
+                      """
+        let alert = UIAlertController(
+                title: "Submit", 
+                message: message,
+                preferredStyle: .alert
+        )
         let okAction = UIAlertAction(title: "OK", style: .default)
         alert.addAction(okAction)
         window?.rootViewController?.present(alert, animated: true)
 
-        var info = CreditCardInfo(fullName: creditCardDefaults?.fullName, number: creditCardNumber?.secureText() ?? creditCardDefaults?.number, verificationValue: <#T##SpreedlySecureOpaqueString##CoreSdk.SpreedlySecureOpaqueString#>, year: <#T##Int##Swift.Int#>, month: <#T##Int##Swift.Int#>)
+//        var info = CreditCardInfo(
+//                fullName: creditCardDefaults?.fullName ?? "",
+//                number: creditCardNumber?.secureText() ?? creditCardDefaults?.number,
+//                verificationValue: <#T##SpreedlySecureOpaqueString##CoreSdk.SpreedlySecureOpaqueString#>,
+//                year: <#T##Int##Swift.Int#>,
+//                month: <#T##Int##Swift.Int#>
+//        )
     }
 
     func keyToView(_ key: String) -> UIView? {
@@ -48,16 +79,21 @@ public class SPSecureTextField: UITextField {
 }
 
 
-extension UIView {
+extension UITextField  {
 
     func text() -> String? {
-        if let tf = self as? UITextField {
-            return tf.text
-        }
-        return nil
+        self.text
+//        if let tf = self as? UITextField {
+//            return tf.text
+//        }
+//        return nil
     }
 
     func secureText() -> SpreedlySecureOpaqueString? {
-        return nil
+        let client = createSpreedlyClient(envKey: "secretEnvKey", envSecret: "secretEnvSecret")
+        guard let text = self.text else {
+            return nil
+        }
+        return client.createSecureString(from: text)
     }
 }
