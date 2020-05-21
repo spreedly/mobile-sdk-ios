05db76fc0251ce1ccc9596eb14fafa771b42c6f4
Eli Thompson <eli@eliandlyndi.com>
2020-05-21 11:21:59 -0700

    can submit form to Spreedly

--- CocoaSample/CreditCardFormViewController.swift
+++ CocoaSample/CreditCardFormViewController.swift
@@ -5,6 +5,7 @@
 import Foundation
 import UIKit
 import CocoaSdk
+import CoreSdk
 
 class CreditCardFormViewController: UIViewController {
 
@@ -12,6 +13,25 @@ class CreditCardFormViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //form?.creditCardDefaults = CreditCardInfo()
+
+        setDefaults()
+    }
+
+    /// Set defaults here for values like name
+    /// or address information.
+    func setDefaults() {
+        form?.fullName?.text = "Dolly Dog"
+
+        let defaults = CreditCardInfo()
+        var billing = Address()
+        billing.address1 = "123 Bark St"
+        billing.city = "Canine"
+        billing.state = "WA"
+        billing.zip = "98121"
+        billing.country = "US"
+        billing.phoneNumber = "206-555-2275"
+        defaults.address = billing
+
+        form?.creditCardDefaults = defaults
     }
 }
--- CocoaSdk/SPSecureForm.swift
+++ CocoaSdk/SPSecureForm.swift
@@ -13,33 +13,65 @@ public protocol SPSecureFormDelegate {
 
 public enum SPSecureClientError: Error {
     case noSpreedlyClient
+    case noSpreedlyCredentials
 }
 
 public class SPSecureForm: UIView {
 
     public var delegate: SPSecureFormDelegate?
 
-    private var _client: SpreedlyClient?
-    public var client: SpreedlyClient {
-        get {
-            if let client = _client {
-                return client
-            } else {
-                if let path = Bundle.main.path(forResource: "Spreedly-env", ofType: "plist"),
-                   let config = NSDictionary(contentsOfFile: path) as? [String: Any],
-                   let envKey = config["ENV_KEY"] as? String,
-                   let envSecret = config["ENV_SECRET"] as? String
-                {
-                    _client = createSpreedlyClient(envKey: envKey, envSecret: envSecret, test: config["TEST"] as? Bool ?? false)
-                    return _client!
-                }
-            }
-            fatalError("SPSecureForm needs a SpreedlyClient, you can either create one and set the client property or you can use a Spreedly-env.plist file.")
+    private func getCredentials() throws -> (envKey: String, envSecret: String, test: Bool) {
+        guard let path = Bundle.main.path(forResource: "Spreedly-env", ofType: "plist"),
+              let config = NSDictionary(contentsOfFile: path) as? [String: Any],
+              let envKey = config["ENV_KEY"] as? String,
+              let envSecret = config["ENV_SECRET"] as? String,
+              envKey.count > 0 && envSecret.count > 0 else {
+            throw SPSecureClientError.noSpreedlyCredentials
         }
-        set {
-            _client = newValue
-        }
+
+        return (
+                envKey: envKey,
+                envSecret: envSecret,
+                test: config["TEST"] as? Bool ?? false
+        )
     }
+
+    private var _client: SpreedlyClient?
+    private func getOrCreateClient() throws -> SpreedlyClient {
+        if let client = _client {
+            return client
+        }
+
+        let credentials = try getCredentials()
+        let client = createSpreedlyClient(
+                envKey: credentials.envKey,
+                envSecret: credentials.envSecret,
+                test: credentials.test
+        )
+        _client = client
+        return client
+    }
+
+//    public var client: SpreedlyClient {
+//        get {
+//            if let client = _client {
+//                return client
+//            } else {
+//                if let path = Bundle.main.path(forResource: "Spreedly-env", ofType: "plist"),
+//                   let config = NSDictionary(contentsOfFile: path) as? [String: Any],
+//                   let envKey = config["ENV_KEY"] as? String,
+//                   let envSecret = config["ENV_SECRET"] as? String
+//                {
+//                    _client = createSpreedlyClient(envKey: envKey, envSecret: envSecret, test: config["TEST"] as? Bool ?? false)
+//                    return _client!
+//                }
+//            }
+//            fatalError("SPSecureForm needs a SpreedlyClient, you can either create one and set the client property or you can use a Spreedly-env.plist file.")
+//        }
+//        set {
+//            _client = newValue
+//        }
+//    }
 
     public var creditCardDefaults: CreditCardInfo?
     public var bankAccountDefaults: BankAccountInfo?
@@ -50,6 +82,8 @@ public class SPSecureForm: UIView {
     @IBOutlet public weak var expirationMonth: UITextField?
     @IBOutlet public weak var expirationYear: UITextField?
 
+    @IBOutlet public weak var errors: UILabel?
+
     public override init(frame: CGRect) {
         super.init(frame: frame)
     }
@@ -59,29 +93,56 @@ public class SPSecureForm: UIView {
     }
 
 
+
     @IBAction public func createCreditCardPaymentMethod(sender: UIView) {
+        let client: SpreedlyClient
+        do {
+            client = try getOrCreateClient()
+        } catch SPSecureClientError.noSpreedlyCredentials  {
+            displayAlert(message: "No credentials were specified.", title: "Error")
+            return
+        } catch {
+            displayAlert(message: "Error: \(error)", title: "Error")
+            return
+        }
+
         let message = """
                       \(fullName?.text() ?? "No name")
                       \(creditCardNumber?.text() ?? "")
                       \(creditCardVerificationNumber?.text() ?? "")
                       \(expirationMonth?.text() ?? "mm")/\(expirationYear?.text() ?? "yy")
                       """
+        displayAlert(message: message, title: "Submit")
+
+        let info = CreditCardInfo(from: creditCardDefaults)
+
+        info.fullName = fullName?.text()
+        info.number = creditCardNumber?.secureText()
+        info.verificationValue = creditCardVerificationNumber?.secureText()
+        info.month = Int(expirationMonth?.text() ?? "")
+        info.year = Int(expirationYear?.text() ?? "")
+
+        _ = client.createCreditCardPaymentMethod(creditCard: info).subscribe(onSuccess: { transaction in
+            if let errors = transaction.errors, errors.count > 0 {
+                DispatchQueue.main.async {
+                    self.errors?.text = errors[0].message    // Update UI
+                }
+                return
+            }
+
+            self.delegate?.spreedly(secureForm: self, creditCardSuccess: transaction)
+        })
+    }
+
+    func displayAlert(message: String, title: String) {
         let alert = UIAlertController(
-                title: "Submit",
+                title: title,
                 message: message,
                 preferredStyle: .alert
         )
         let okAction = UIAlertAction(title: "OK", style: .default)
         alert.addAction(okAction)
         window?.rootViewController?.present(alert, animated: true)
-
-//        var info = CreditCardInfo(
-//                fullName: creditCardDefaults?.fullName ?? "",
-//                number: creditCardNumber?.secureText() ?? creditCardDefaults?.number,
-//                verificationValue: <#T##SpreedlySecureOpaqueString##CoreSdk.SpreedlySecureOpaqueString#>,
-//                year: <#T##Int##Swift.Int#>,
-//                month: <#T##Int##Swift.Int#>
-//        )
     }
 
     func keyToView(_ key: String) -> UIView? {
