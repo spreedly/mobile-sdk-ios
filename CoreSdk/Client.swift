86d3ea648a7fa26bc49c8fd3901298b5908d954e
Eli Thompson <eli@ergonlabs.com>
2020-06-10 20:46:35 -0700

    add ClientFactory.swift

--- CoreSdk/Client.swift
+++ CoreSdk/Client.swift
@@ -53,15 +53,3 @@ public protocol _ObjCClient {
             from: PaymentMethodRequestBase
     ) -> _ObjCSingleTransaction
 }
-
-@objc(SPRClientFactory)
-public class ClientFactory: NSObject {
-    public static func create(envKey: String, envSecret: String, test: Bool = false) -> SpreedlyClient {
-        SpreedlyClientImpl(envKey: envKey, envSecret: envSecret, test: test)
-    }
-
-    @objc(createWithEnvKey:envSecret:test:)
-    public static func _objCCreate(envKey: String, envSecret: String, test: Bool) -> _ObjCClient {
-        SpreedlyClientImpl(envKey: envKey, envSecret: envSecret, test: test)
-    }
-}
