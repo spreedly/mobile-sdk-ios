c06780548d038385659fdb0a37758cbdadf5397b
Eli Thompson <eli@ergonlabs.com>
2020-07-02 17:40:14 -0700

    use swiftlint config

--- lint.sh
+++ lint.sh
@@ -1,15 +1,3 @@
 #!/usr/bin/env bash
 
-pushd CoreSdk
-../Pods/SwiftLint/swiftlint
-popd
-pushd SdkTests
-../Pods/SwiftLint/swiftlint
-popd
-pushd SdkIntegraionTests
-../Pods/SwiftLint/swiftlint
-popd
-pushd Sample
-../Pods/SwiftLint/swiftlint
-popd
-
+./Pods/SwiftLint/swiftlint --config .swiftlint.yml
