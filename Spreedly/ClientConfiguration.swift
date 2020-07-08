dceb48286e009ccf91b01c67cb4715fd99bbef66
Eli Thompson <eli@ergonlabs.com>
2020-07-08 12:38:19 -0700

    add ClientConfiguration.swift

---
+++ Spreedly/ClientConfiguration.swift
@@ -0,0 +1,41 @@
+//
+// Created by Eli Thompson on 7/8/20.
+//
+
+import Foundation
+
+@objc(SPRClientConfiguration)
+public class ClientConfiguration: NSObject {
+    @objc public let envKey: String
+    @objc public var envSecret: String?
+    @objc public var test: Bool = false
+    @objc public var testCardNumber: String?
+
+    @objc public init(envKey: String, envSecret: String? = nil, test: Bool = false, testCardNumber: String? = nil) {
+        self.envKey = envKey
+        self.envSecret = envSecret
+        self.test = test
+        self.testCardNumber = testCardNumber
+    }
+
+    @objc public static func getConfiguration() throws -> ClientConfiguration {
+        guard let path = Bundle.main.path(forResource: "Spreedly-env", ofType: "plist"),
+              let config = NSDictionary(contentsOfFile: path) as? [String: Any],
+              let envKey = config["ENV_KEY"] as? String,
+              !envKey.isEmpty else {
+            throw ClientError.noSpreedlyCredentials
+        }
+
+        return ClientConfiguration(
+                envKey: envKey,
+                envSecret: config["ENV_SECRET"] as? String,
+                test: config["TEST"] as? Bool ?? false,
+                testCardNumber: config["TEST_CARD_NUMBER"] as? String
+        )
+    }
+}
+
+@objc(SPRClientError)
+public enum ClientError: Int, Error {
+    case noSpreedlyCredentials
+}
