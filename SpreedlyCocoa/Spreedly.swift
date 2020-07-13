2caab9f19909a870276e79aee19d4d9b544e2c05
Eli Thompson <eli@ergonlabs.com>
2020-07-13 12:24:19 -0700

    Code docs for ExpressBuilder

--- SpreedlyCocoa/Spreedly.swift
+++ SpreedlyCocoa/Spreedly.swift
@@ -17,6 +17,19 @@ public class Spreedly: NSObject {
     }
 }
 
+/// ExpressBuilder is the entrypoint for the Express UI workflow. Create an instance of this class, optionally set
+/// properties to configure Express UI behavior, and finally call `buildViewController()` to get a `UIViewController`
+/// ready to show.
+///
+/// ### Example
+/// ```swift
+/// var builder = ExpressBuilder()
+/// builder.didSelectPaymentMethod = { item in
+///     print("User wants to use a payment method with token: \(item.token)")
+/// }
+/// let controller = builder.buildViewController()
+/// navigationController?.show(controller, sender: self)
+/// ```
 @objc(SPRExpressBuilder)
 public class ExpressBuilder: NSObject {
     @objc(SPRPresentationStyle)
@@ -28,10 +41,21 @@ public class ExpressBuilder: NSObject {
         case asModal
     }
 
+    /// Default: true. Allow the user to add new card payment methods.
     @objc public var allowCard = true
+
+    /// Default: false. Allow the user to add new bank account payment methods.
     @objc public var allowBankAccount = false
+
+    /// Default: true. Allow users to select the Apple Pay payment method. When true, the `paymentRequest` property
+    /// must also be set.
     @objc public var allowApplePay = true
+
+    /// A list of payment methods available for the user to select. Set if providing
+    /// any previously stored payment methods.
     @objc public var paymentMethods: [PaymentMethodItem]?
+
+    /// Called after the user selects a payment method.
     @objc public var didSelectPaymentMethod: ((PaymentMethodItem) -> Void)?
 
     /// Set this to provide a full name on the payment method creation forms and to provide
@@ -54,16 +78,47 @@ public class ExpressBuilder: NSObject {
     /// when the payment method is created. When this property is set, `defaultPaymentMethodInfo` will be ignored.
     @objc public var defaultApplePayInfo: PaymentMethodInfo?
 
+    /// Required when offering Apple Pay. Provides values to the Apple Pay dialog.
+    ///
+    /// ### Example
+    /// ```swift
+    /// let request = PKPaymentRequest()
+    /// request.merchantIdentifier = "merchant.com.your_company.app"
+    /// request.countryCode = "US"
+    /// request.currencyCode = "USD"
+    /// request.supportedNetworks = [.amex, .discover, .masterCard, .visa]
+    /// request.merchantCapabilities = [.capabilityCredit, .capabilityDebit]
+    /// request.paymentSummaryItems = [
+    ///     PKPaymentSummaryItem(label: "Amount", amount: NSDecimalNumber(string: "322.38"), type: .final),
+    ///     PKPaymentSummaryItem(label: "Tax", amount: NSDecimalNumber(string: "32.24"), type: .final),
+    ///     PKPaymentSummaryItem(label: "Total", amount: NSDecimalNumber(string: "354.62"), type: .final)
+    /// ]
+    ///
+    /// let builder = ExpressBuilder()
+    /// builder.paymentRequest = request
+    /// ```
     @objc public var paymentRequest: PKPaymentRequest?
 
+    /// Default `.withinNavigationView`. When showing the Express UI within an existing `UINavigationController`,
+    /// set this to `.withinNavigationView`. When it is meant to appear separately, such as within a modal,
+    /// set this to `.asModal` so it will provide its own `UINavigationController`.
     @objc public var presentationStyle: PresentationStyle = .withinNavigationView
 
+    /// Appears at the top of the payment selection screen. Also set `paymentSelectionHeaderHeight` when
+    /// using this property.
     @objc public var paymentSelectionHeader: UIView?
+
+    /// Default zero. Set this to more than zero when using the `paymentSelectionHeader`.
     @objc public var paymentSelectionHeaderHeight: CGFloat = 0
 
+    /// Appears after all other controls on the payment selection screen. Also set `paymentSelectionFooterHeight` when
+    /// using this property.
     @objc public var paymentSelectionFooter: UIView?
+
+    /// Default zero. Set this to more than zero when using the `paymentSelectionFooter`.
     @objc public var paymentSelectionFooterHeight: CGFloat = 0
 
+    /// Returns a `UIViewController` for the Express UI workflow configured with the properties from this object.
     public func buildViewController() -> UIViewController {
         let bundle = Bundle(for: type(of: self))
         let storyboard = UIStoryboard(name: "Express", bundle: bundle)
