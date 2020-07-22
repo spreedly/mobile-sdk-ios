57f5fae394bbed18780ac3f8b45232966832a944
Eli Thompson <eli@ergonlabs.com>
2020-07-22 09:40:42 -0700

    text can be turned into an image

--- SpreedlyCocoa/String+Additions.swift
+++ SpreedlyCocoa/String+Additions.swift
@@ -3,6 +3,7 @@
 //
 
 import Foundation
+import UIKit
 
 extension String {
     func onlyNumbers() -> String {
@@ -11,5 +12,20 @@ extension String {
 
     func withoutSpaces() -> String {
         self.replacingOccurrences(of: " ", with: "")
+    }
+
+    func image(fontSize: CGFloat = 40, bgColor: UIColor = UIColor.clear, imageSize: CGSize? = nil) -> UIImage? {
+        let font = UIFont.systemFont(ofSize: fontSize)
+        let attributes = [NSAttributedString.Key.font: font]
+        let imageSize = imageSize ?? self.size(withAttributes: attributes)
+
+        UIGraphicsBeginImageContextWithOptions(imageSize, false, 0)
+        bgColor.set()
+        let rect = CGRect(origin: .zero, size: imageSize)
+        UIRectFill(rect)
+        self.draw(in: rect, withAttributes: [.font: font])
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
     }
 }
