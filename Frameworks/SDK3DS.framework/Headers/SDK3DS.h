9736d6c142d841e2437b9eb19ef43ddbc1ef6ba5
Stefan Rusek <stefan@rusek.org>
2020-10-21 16:54:14 -0400

    add seglan library

---
+++ Frameworks/SDK3DS.framework/Headers/SDK3DS.h
@@ -0,0 +1,18 @@
+//
+//  SDK3DS.h
+//  SDK3DS
+//
+//  Created by Inaki Garaizabal on 14/03/2019.
+//  Copyright © 2019 Seglan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+//! Project version number for SDK3DS.
+FOUNDATION_EXPORT double SDK3DSVersionNumber;
+
+//! Project version string for SDK3DS.
+FOUNDATION_EXPORT const unsigned char SDK3DSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDK3DS/PublicHeader.h>
+
+
