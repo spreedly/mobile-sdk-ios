50a97895eeb19768a3f02a28d8537c88331d839e
Stefan Rusek <stefan@rusek.org>
2021-12-10 11:10:35 -0500

    version 1.0.0-beta8

---
+++ Spreedly-Release.xcframework/ios-arm64_x86_64-simulator/Spreedly.framework/Headers/Spreedly.h
@@ -0,0 +1,19 @@
+//
+//  Spreedly.h
+//  Spreedly
+//
+//  Created by Eli Thompson on 4/23/20.
+//  Copyright © 2020 Spreedly. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Spreedly.
+FOUNDATION_EXPORT double SpreedlyVersionNumber;
+
+//! Project version string for Spreedly.
+FOUNDATION_EXPORT const unsigned char SpreedlyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Spreedly/PublicHeader.h>
+
+
