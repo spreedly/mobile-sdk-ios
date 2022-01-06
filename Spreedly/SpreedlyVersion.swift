47c8513ffbe5b987be86ac45f4b89dd33ac79517
Stefan Rusek <stefan@rusek.org>
2022-01-06 17:36:10 -0500

    Platform data

---
+++ Spreedly/SpreedlyVersion.swift
@@ -0,0 +1,11 @@
+// DO NOT EDIT/n// Edit the version in version.podspec
+
+import Foundation
+
+class SpreedlyVersion {
+    static let podVersion = "1.0.0-beta9"
+    static let gitBranch = "master";
+    static let gitTag = "";
+    static let gitCommitId = "9b1045e9f80bafe237a54ae1073448fafe4f1cae";
+}
+
