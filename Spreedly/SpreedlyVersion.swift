dc93104430b656b06f95ae6eda0529fd2c1386a8
Stefan Rusek <stefan@rusek.org>
2022-03-10 17:28:21 -0500

    force le bitcode

--- Spreedly/SpreedlyVersion.swift
+++ Spreedly/SpreedlyVersion.swift
@@ -6,6 +6,6 @@ class SpreedlyVersion {
     static let podVersion = "1.0.0-beta14"
     static let gitBranch = "master";
     static let gitTag = "";
-    static let gitCommitId = "65fcd902b97310e6f6bee8e413b7412adecdae6c";
+    static let gitCommitId = "48f7fcf48a9366753c313b88e8acecf976e72b5a";
 }
 
